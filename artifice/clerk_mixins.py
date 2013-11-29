f93e20b5297aa9249ab7fe3990ca7b47eb6ecb3b
adrian <adriant@catalyst.net.nz>
2013-11-29 14:37:43 +1300

    Added a couple of mixin classes to grab data from the webservice.

---
+++ artifice/clerk_mixins.py
@@ -0,0 +1,32 @@
+import requests
+from decimal import Decimal
+# from artifice import NotFound
+
+
+class ClerkNamesMixin(object):
+
+    def pretty_name(self, name):
+        url = self.config["clerk"]["url"]
+        url = url + "service_types/" + name + "/"
+        response = requests.get(url,
+                                headers={"Content-Type": "application/json"})
+        if response.status_code == 200:
+            return str(response.json()['pretty_name'])
+        elif response.status_code == 404:
+            # raise NotFound
+            print "not found"
+
+
+class ClerkRatesMixin(object):
+
+    def rate(self, name, loc_name):
+        url = self.config["clerk"]["url"]
+        url = (url + "locations/" + loc_name +
+               "/services/" + name + "/rates/current/")
+        response = requests.get(url,
+                                headers={"Content-Type": "application/json"})
+        if response.status_code == 200:
+            return Decimal(response.json()['rate'])
+        elif response.status_code == 404:
+            # raise NotFound
+            print "not found"
