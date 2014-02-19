cedd8dd41e759aa24c675ef45d2fc14d5b913335
adriant <adriant@catalyst.net.nz>
2014-02-20 10:47:45 +1300

    adding more arguments to shell.py, that default to env variables.

--- client/shell.py
+++ client/shell.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# import sys
+import os
 from client import Client
 
 # import yaml
@@ -9,12 +9,39 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
 
-    subparsers = parser.add_subparsers(help='commands', dest='command')
+    parser.add_argument('--os-username',
+                        default=os.environ.get('OS_USERNAME'),
+                        help='Defaults to env[OS_USERNAME]')
 
-    parser.add_argument(
-        "-c", "--config", dest="config",
-        help="Config file",
-        default="/etc/artifice/conf.yaml")
+    parser.add_argument('--os-password',
+                        default=os.environ.get('OS_PASSWORD'),
+                        help='Defaults to env[OS_PASSWORD]')
+
+    parser.add_argument('--os-tenant-id',
+                        default=os.environ.get('OS_TENANT_ID'),
+                        help='Defaults to env[OS_TENANT_ID]')
+
+    parser.add_argument('--os-tenant-name',
+                        default=os.environ.get('OS_TENANT_NAME'),
+                        help='Defaults to env[OS_TENANT_NAME]')
+
+    parser.add_argument('--os-auth-url',
+                        default=os.environ.get('OS_AUTH_URL'),
+                        help='Defaults to env[OS_AUTH_URL]')
+
+    parser.add_argument('--os-region-name',
+                        default=os.environ.get('OS_REGION_NAME'),
+                        help='Defaults to env[OS_REGION_NAME]')
+
+    parser.add_argument('--os-auth-token',
+                        default=os.environ.get('OS_AUTH_TOKEN'),
+                        help='Defaults to env[OS_AUTH_TOKEN]')
+
+    parser.add_argument("-c", "--config", dest="config",
+                        help="Config file",
+                        default="/etc/artifice/conf.yaml")
+
+    subparsers = parser.add_subparsers(help='commands', dest='command')
 
     usage_parser = subparsers.add_parser('usage', help=('process usage' +
                                                         ' for given tenants'))
