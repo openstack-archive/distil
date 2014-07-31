fe6691e7335adcfe58da306fe18736f406b2795d
adriant <adriant@catalyst.net.nz>
2014-07-31 17:18:20 +1200

    Fixing model tests to except correct exceptions
    Also pep8 clean-up of model tests.
    
    Change-Id: Idda2802e78203de055ec939d92c0ce59830abe3a

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,16 +1,17 @@
 import unittest
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session,create_session
+from sqlalchemy.orm import scoped_session, create_session
 from sqlalchemy.pool import NullPool
-from distil.models import Resource, Tenant, UsageEntry, SalesOrder, Base
+from sqlalchemy.exc import IntegrityError, OperationalError
+from distil.models import Resource, Tenant, UsageEntry, SalesOrder
 import datetime
-import subprocess
 
-from . import DATABASE_NAME, PG_DATABASE_URI, MY_DATABASE_URI
+from . import PG_DATABASE_URI, MY_DATABASE_URI
 
 
 pg_engine = None
 mysql_engine = None
+
 
 def setUp():
     global mysql_engine
@@ -23,9 +24,11 @@ def tearDown():
     pg_engine.dispose()
     mysql_engine.dispose()
 
+
 class db(unittest.TestCase):
-    
+
     __test__ = False
+
     def setUp(self):
         self.db = self.session()
 
@@ -66,13 +69,13 @@ class db(unittest.TestCase):
     def test_insert_usage_entry(self):
         self.test_create_resource()
         self.db.begin()
-        t = self.db.query(Tenant).get("asfd")
         r = self.db.query(Resource).filter(Resource.id == "1234")[0]
         u = UsageEntry(service="cheese",
                        volume=1.23,
                        resource=r,
                        tenant=r,
-                       start=datetime.datetime.utcnow() - datetime.timedelta(minutes=5),
+                       start=(datetime.datetime.utcnow() -
+                              datetime.timedelta(minutes=5)),
                        end=datetime.datetime.utcnow(),
                        created=datetime.datetime.utcnow())
         self.db.add(u)
@@ -80,13 +83,14 @@ class db(unittest.TestCase):
             self.db.commit()
         except Exception as e:
             self.fail("Exception: %s" % e)
+
     def test_overlapping_usage_entry_fails(self):
         self.test_insert_usage_entry()
         try:
             self.test_insert_usage_entry()
             # we fail here
             self.fail("Inserted overlapping row; failing")
-        except Exception as e:
+        except (IntegrityError, OperationalError):
             self.db.rollback()
             self.assertEqual(self.db.query(UsageEntry).count(), 1)
 
@@ -94,29 +98,32 @@ class db(unittest.TestCase):
         self.test_insert_usage_entry()
         self.db.begin()
         usage = self.db.query(UsageEntry)[0]
-        tenant =self.db.query(Tenant).get("asfd") 
-        so = SalesOrder(tenant = tenant,
-                        start = usage.start,
-                        end = usage.end)
+        tenant = self.db.query(Tenant).get("asfd")
+        so = SalesOrder(tenant=tenant,
+                        start=usage.start,
+                        end=usage.end)
         self.db.add(so)
         self.db.commit()
         so2 = self.db.query(SalesOrder)[0]
         self.assertTrue(so2.tenant.id == so.tenant.id)
         self.assertTrue(so2.start == so.start)
         self.assertTrue(so2.end == so.end)
+
     def test_overlap_sales_order_fails(self):
         self.test_insert_salesorder()
         try:
             self.test_insert_salesorder()
             self.fail("Inserted twice")
-        except Exception as e:
+        except (IntegrityError, OperationalError):
             self.db.rollback()
             self.assertEqual(self.db.query(SalesOrder).count(), 1)
+
 
 class TestDatabaseModelsPostgres(db):
 
     __test__ = True
     session = scoped_session(lambda: create_session(bind=pg_engine))
+
 
 class TestDatabaseModelsMysql(db):
 
