30f37d198112f87dbef1e2cf08244c05bc7e965c
Fei Long Wang <flwang@catalyst.net.nz>
2016-04-27 16:26:22 +1200

    Refactor Distil API to support new endpoints
    Change-Id: I4d456f9228e879234898177d1f2d791d2ffbbf46

---
+++ distil/cli/distil_api.py
@@ -0,0 +1,57 @@
+# Copyright 2014 Catalyst IT Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import sys
+
+import eventlet
+from eventlet import wsgi
+from oslo_config import cfg
+import logging as std_logging
+from distil.api import app
+from oslo_log import log
+from distil import config
+
+
+CONF = cfg.CONF
+LOG = log.getLogger(__name__)
+
+
+class WritableLogger(object):
+    """A thin wrapper that responds to `write` and logs."""
+
+    def __init__(self, LOG, level=std_logging.DEBUG):
+        self.LOG = LOG
+        self.level = level
+
+    def write(self, msg):
+        self.LOG.log(self.level, msg.rstrip("\n"))
+
+
+def main():
+    CONF(project='distil', prog='distil-api')
+    log.setup(CONF, 'distil')
+
+    application = app.make_app()
+    CONF.log_opt_values(LOG, logging.INFO)
+    try:
+        wsgi.server(eventlet.listen((CONF.host, CONF.port), backlog=500),
+                    application, log=WritableLogger(LOG))
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
