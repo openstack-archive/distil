3b225826b338e5b86ecf9e8f7a8f7c63f700eccc
Lingxian Kong <anlin.kong@gmail.com>
2016-05-09 22:55:51 +1200

    Adding periodic task mechanism to distil
    Convert collect_usage api interface to periodic task internally.
    
    Change-Id: Ib449c9cac217f0b98d7543918c9b8ca3541b0a10

--- distil/config.py
+++ distil/config.py
@@ -15,10 +15,18 @@
 from oslo_config import cfg
 from oslo_log import log
 
+CONF = cfg.CONF
+
 DEFAULT_OPTIONS = (
     cfg.ListOpt('ignore_tenants', default=[],
                 help=(''),),
 )
+
+COLLECTOR_OPTIONS = [
+    cfg.IntOpt('collect_usage_interval', default=-1,
+               help=('Interval of usage collection. -1 means the collection '
+                     'task will be disabled.')),
+]
 
 ODOO_OPTS = [
     cfg.StrOpt('version', default='8.0',
@@ -38,11 +46,11 @@ ODOO_OPTS = [
 ]
 
 ODOO_GROUP = 'odoo'
+COLLECTOR_GROUP = 'collector'
 
-
-def config_options():
-    return [(None, DEFAULT_OPTIONS),
-            (ODOO_GROUP, ODOO_OPTS)]
+CONF.register_opts(DEFAULT_OPTIONS)
+CONF.register_opts(ODOO_OPTS, group=ODOO_GROUP)
+CONF.register_opts(COLLECTOR_OPTIONS, group=COLLECTOR_GROUP)
 
 # This is simply a namespace for global config storage
 main = None
