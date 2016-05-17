bc8c97f2dc98f975d3d8aada4e0795aadd436661
Lingxian Kong <anlin.kong@gmail.com>
2016-05-17 23:09:30 +1200

    Refactor collect_usage function
    TODO:
    - implement get_meter function in CeilometerCollector
    - implement _get_projects function in CollectorService
    - implement usages_add function in db layer
    - split tenants handling in different thread
    - more rigorous testing
    
    Change-Id: I0235c78c7dc520e4bf08405ebf8e06875868daab

--- distil/collector/ceilometer.py
+++ distil/collector/ceilometer.py
@@ -15,10 +15,36 @@
 from oslo_config import cfg
 from oslo_log import log as logging
 
+from distil.collector import base
+
 LOG = logging.getLogger(__name__)
 CONF = cfg.CONF
 
 
-class CeilometerCollector(object):
+class CeilometerCollector(base.BaseCollector):
     def __init__(self, *arg, **kwargs):
+        super(CeilometerCollector, self).__init__(*arg, **kwargs)
+
+    def _get_client(self):
         pass
+
+    def get_meter(self, project_id, meter, start, end):
+        return [
+            {
+                "id": "9b23b398-6139-11e5-97e9-bc764e045bf6",
+                "resource_metadata": {
+                    "availability_zone": "zone1",
+                    "display_name": "volume_name"
+                },
+                "meter": "volume.size",
+                "project_id": "35b17138-b364-4e6a-a131-8f3099c5be68",
+                "recorded_at": "2015-09-22T14:52:54.850725",
+                "resource_id": "bd9431c1-8d69-4ad3-803a-8d4a6b89fd36",
+                "source": "openstack",
+                "timestamp": "2015-09-22T14:52:54.850718",
+                "type": "gauge",
+                "unit": "GB",
+                "user_id": "efd87807-12d2-4b38-9c70-5f5c2ac427ff",
+                "counter_volume": 1
+            }
+        ]
