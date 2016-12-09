57872fa2f17c1fb4fcb8c2ea1b0330f7ed1b4ad7
Fei Long Wang <flwang@catalyst.net.nz>
2016-12-09 16:48:56 +1300

    Add the support for cache
    This is a new feature to improve the performance for API request,
    since most of the API endpoints of Distil can be cached by parameters.
    
    Change-Id: I2155137a0e9965215f359a20067c4b2065e4de11

---
+++ distil/tests/unit/utils/test_cache.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2014 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import flask
+import mock
+from oslo_cache import core
+from oslo_config import cfg
+
+from distil import exceptions as ex
+from distil.utils import cache
+from distil.tests.unit import base
+
+
+class TestCache(base.DistilTestCase):
+
+    config_file = 'distil_cache.conf'
+
+    def setUp(self):
+        super(TestCache, self).setUp()
+        self._set_cache(self.conf)
+
+    def _set_cache(self, conf):
+        cache.setup_cache(conf)
+
+    def test_cache(self):
+        count = 0
+        @cache.memoize
+        def test(name):
+            if count == 0:
+                return "hello, Tom"
+            if count == 1:
+                return "morning, Tom"
+            if count == 2:
+                return "evening, Tom"
+
+        name = 'Tom'
+        for x in xrange(0, 2):
+            self.assertEqual(test(name), 'hello, Tom')
