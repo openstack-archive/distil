ebe2d73efaca50b9c900cf19b9a89dbf3a7d5243
Feilong Wang <flwang@catalyst.net.nz>
2019-02-02 14:16:32 +1300

    Support JSON file as an ERP driver
    Change-Id: I080082a9d8e2f458336985ca996fdf3d71a7d629

--- distil/config.py
+++ distil/config.py
@@ -115,6 +115,16 @@ ODOO_OPTS = [
                      'c1.c1r2-windows or c1.c2r4-sql-server-standard-windows')
 ]
 
+JSONFILE_OPTS = [
+    cfg.StrOpt('products_file_path',
+               default='/etc/distil/products.json',
+               help='Json file to contain the products and prices.'),
+    cfg.ListOpt('extra_product_category_list',
+                default=[],
+                help='Additional product categories which should be easily '
+                     'parsed. Such as new product category created in file.'),
+]
+
 
 CLI_OPTS = [
     cfg.StrOpt(
@@ -128,10 +138,12 @@ CLI_OPTS = [
 AUTH_GROUP = 'keystone_authtoken'
 ODOO_GROUP = 'odoo'
 COLLECTOR_GROUP = 'collector'
+JSONFILE_GROUP = 'jsonfile'
 
 
 CONF.register_opts(DEFAULT_OPTIONS)
 CONF.register_opts(ODOO_OPTS, group=ODOO_GROUP)
+CONF.register_opts(JSONFILE_OPTS, group=JSONFILE_GROUP)
 CONF.register_opts(COLLECTOR_OPTS, group=COLLECTOR_GROUP)
 CONF.register_cli_opts(CLI_OPTS)
 
